650641e54a2250a940e3770cfbc9ed703679a00d
jstebel <jan.stebel@tul.cz>
2024-08-12 12:39:29 +0200

    Fix unit in comment.

--- notebooks/Richards/toy_parflow.py
+++ notebooks/Richards/toy_parflow.py
@@ -76,7 +76,7 @@ class ToyProblem:
         self._run.Geom.Perm.Names = "domain"
 
         self._run.Geom.domain.Perm.Type = "Constant"
-        self._run.Geom.domain.Perm.Value = 30.8 / 100 / 24 # 1.2833e-2 [cm/d] -> [m/s]
+        self._run.Geom.domain.Perm.Value = 30.8 / 100 / 24 # 1.2833e-2 [cm/d] -> [m/h]
 
         self._run.Perm.TensorType = "TensorByGeom"
         self._run.Geom.Perm.TensorByGeom.Names = "domain"
